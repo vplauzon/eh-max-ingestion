dc2224b50e29ed8d1ec5c964cba1eb610d10522d
vplauzon <vincent_lauzon@yahoo.com.au>
2023-09-15 17:47:43 -0400

    add deploy

---
+++ deployment/deploy-infra.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+##########################################################################
+##  Deploys Azure infrastructure
+
+rg=$1
+
+echo "Resource group:  $rg"
+echo "Current directory:  $(pwd)"
+
+echo
+echo "Deploying ARM template"
+
+az deployment group create -n "deploy-$(uuidgen)" -g $rg \
+    --template-file main.bicep
