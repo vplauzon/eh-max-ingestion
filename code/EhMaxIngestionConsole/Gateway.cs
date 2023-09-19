f2b592f18698a3a89f92b2e2f35a1621b20c66c1
vplauzon <vincent_lauzon@yahoo.com.au>
2023-09-19 14:49:52 -0400

    .

---
+++ code/EhMaxIngestionConsole/Gateway.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EhMaxIngestionConsole
+{
+    internal class Gateway
+    {
+        private const int DRONE_COUNT = 10;
+
+        public string GatewayId { get; } = Guid.NewGuid().ToString();
+
+        public IImmutableList<string> DroneIds { get; } = Enumerable
+            .Range(0, DRONE_COUNT)
+            .Select(i => Guid.NewGuid().ToString())
+            .ToImmutableArray();
+    }
+}
